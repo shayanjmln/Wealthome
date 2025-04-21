0246b00491b538b1a7396ccbcfe5cd070ebf8cfb
shayanjmln <shayan.jamalian98@gmail.com>
2025-04-21 14:41:37 +0430

    Add to favorite Buttons toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,15 @@ const /** {NodeElement} */ $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+
+/**
+ * Add to favorite Buttons toggle
+ */
+const /** {NodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+});
